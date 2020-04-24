4dd1dd491e1f760eba98fc60435eef4a52d762bd
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-04-24 12:09:55 -0300

    Input & Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './style';
 import logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import { Container, Title } from './style';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logo} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('XXX');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
