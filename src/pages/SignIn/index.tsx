935aca1b16bed52a63dd764b6978b5b228c25e71
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-04-24 10:45:24 -0300

    Densidade de pixel

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { Container } from './style';
+import logo from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logo} />
+    </Container>
+  );
 };
 
 export default SignIn;
