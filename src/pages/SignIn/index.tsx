ad9039aebd83f96f54859976c2961824b41f878d
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-04-24 15:32:30 -0300

    Tela de autenticação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,65 @@
 import React from 'react';
-import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  View,
+  ScrollView,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import logo from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container, Title } from './style';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './style';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
-      <Title>Faça seu logon</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logo} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('XXX');
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
         onPress={() => {
-          console.log('XXX');
+          console.log('tira depois');
         }}
       >
-        Entrar
-      </Button>
-    </Container>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
