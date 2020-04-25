4a5fd65f908bedeb1cadac450e6bbb7cf2b0a4b1
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-04-25 08:43:47 -0300

    Rotas privadas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
